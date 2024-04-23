019d7e7ec38ec2116b8e0f03da44a8b8ca4d6204
steve <you@example.com>
2024-04-23 10:23:31 +0100

    cleanup

--- Visualisations/app.py
+++ Visualisations/app.py
@@ -1,5 +1,5 @@
 import torch
-from flask import Flask, render_template, request, jsonify, send_file, make_response
+from flask import Flask, render_template, request, jsonify
 import sys
 sys.path.append(".")
 sys.path.append("..")
@@ -11,11 +11,8 @@ import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 from base64 import encodebytes
-from flask import jsonify
-#make a dictionary of all the functions we want to use
 
 functions=get_all_LSA_fns()
-#normedfunctions={i:get_lsa_fn(i) for i in range(1,17)}
 app = Flask(__name__,template_folder='.')
 
 
@@ -26,10 +23,8 @@ def convert_to_fp8_e4m3(T):
 
     
 def square(logits,img_buf = BytesIO()):
-    #a function that takes numpy logits and plots them on an x and y axis
     plt.figure(figsize=(logits.shape[0],logits.shape[1]))
     plt.imshow(logits)
-    #do not display the graph, but save it to a buffer
     plt.savefig(img_buf, format='png')
     encoded_img = encodebytes(img_buf.getvalue()).decode('ascii') # encode as base64
 
@@ -37,11 +32,7 @@ def square(logits,img_buf = BytesIO()):
 
     
 def draw(logits,buffer=BytesIO()):
-    # Defining the side of the cube
-    sides = len(logits.shape) #(subtract to take slices)
-    # Calling the cubes () function
-    #cubes(sides)
-    
+    sides = len(logits.shape)
     if sides==2:
         return square(logits,buffer)
 
@@ -58,7 +49,6 @@ def attempt(func,x):
 
         return torch.zeros_like(x)
 def process(x):
-    #take the string value x and convert it to a float
     try:
         return float(x)
     except:
@@ -68,16 +58,11 @@ def process(x):
 @app.route('/lsa/data', methods=['GET','POST'])
 async def getplots():
 
-    # print("request",request.get_data())
     data=request.get_json()
-    #convert from list of list of strings to list of list of floats to a tensor 
-    #any nan values are converted to 0 and remove non-numeric values
     values=[[process(x) for x in row] for row in data["values"]]
     maximize=data["maximize"]
     fp8=data["precision"]
     x=torch.tensor(values,dtype=torch.float32)
-    #logging.warning("values"+str(values))
-    #log size of x to console 
     if fp8=="E5M2":
         x=convert_to_fp8_e5m2(x)
 
@@ -85,13 +70,7 @@ async def getplots():
         x=convert_to_fp8_e4m3(x)
 
     out={}
-    # check if x is square i.e shape[0]==shape[1]
     outputs={name:func(x,maximize=maximize) for name,func in functions.items()}
-    # for i in outputs.values():
-    #         logging.warning(i)
-    # for lossname,func in get_all_loss_fns().items():
-    #     out.update({"{} with {} loss".format(name,lossname): str(loss(outputs[lossname],x,app))} for name,_ in functions.items())
-    #out.update({str(name) + " loss": str(loss(outputs[name],x,app)) for name,_ in functions.items()})
     out.update({str(name) + " LSA score": torch.sum(x*value).item() for name,value in outputs.items()})
     out.update({str(name): value.tolist() for name,value in outputs.items()})
     output=jsonify(out)
